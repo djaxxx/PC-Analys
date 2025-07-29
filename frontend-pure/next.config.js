a1e8f53a7877e750d33dcd9f8353bd70bca90cfb
djaxxx <jacques.tirot@epitech.digital>
2025-07-29 23:22:30 +0200

    Réintégration de frontend-pure comme dossier normal du projet

---
+++ frontend-pure/next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*', // Backend local !
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+  // Forcer l'utilisation de l'URL relative
+  assetPrefix: '',
+  basePath: '',
+};
+module.exports = nextConfig; 
