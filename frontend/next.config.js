1376b67cda444739f034affc434d5149909ab340
djaxxx <91596765+djaxxx@users.noreply.github.com>
2025-07-30 15:44:55 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*', // Backend local !
+        destination: 'https://ton-backend.up.railway.app/api/:path*', // Backend local !
       },
     ];
   },
